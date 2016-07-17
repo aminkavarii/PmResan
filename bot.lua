8195e3e42db73522775d905266698c91caf0d51d
aminkavarii <amin.kavery.nasab.0631133@gmail.com>
2016-07-17 16:58:52 +0000

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -6,8 +6,8 @@ JSON = require('dkjson')
 HTTPS = require('ssl.https')
 dofile('utilities.lua')
 ----config----
-local bot_api_key = "" --BOT TOKEN تو کن ربات خود را در اینجا قرار دهید
-local You = 188548712  --ID ADMIN ایدی خود را اینجا قرار دهید
+local bot_api_key = "256953986:AAHugpVQTQ80ed50TzePVghSSsE6dvQH7i0" --BOT TOKEN تو کن ربات خود را در اینجا قرار دهید
+local You = 97242093  --ID ADMIN ایدی خود را اینجا قرار دهید
 local BASE_URL = "https://api.telegram.org/bot"..bot_api_key
 local BASE_FOLDER = ""
 local start = [[ ]]
@@ -33,7 +33,7 @@ local help = [[
 `/id` ✴️
 ایدی
 ➖➖➖➖➖➖➖➖➖➖➖
-M.KH @cruel0098
+@TeleGuardTeam
 ]]--
 -------
 
